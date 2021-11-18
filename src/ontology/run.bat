f857e1099db5532e6a69e836045aee8532860075
Berrios <daniel.c.berrios@nasa.gov>
2021-11-18 09:35:18 -0800

    Build of Luke's branch NCBITaxon and ENVO terms

--- src/ontology/run.bat
+++ src/ontology/run.bat
@@ -1,1 +1,1 @@
-docker run -v %cd%\..\..\:/work -w /work/src/ontology --rm -ti obolibrary/odkfull %*
+docker run --env ROBOT_JAVA_ARGS=-Xmx12G -v %cd%\..\..\:/work -w /work/src/ontology --rm -ti --memory=12g obolibrary/odkfull %*
